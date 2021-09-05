9569e7491115b2f4f0b8bb29b5a4ac46886ae9fb
Daniel <daniel.jones424@gmail.com>
2021-09-04 21:30:49 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+var el = document.getElementById('main')
+el.remove();
+
+    const newHeader = document.createElement("h1");
+    newHeader.id = 'victory'
+    newHeader.textContent = 'Daniel is the championa'
